3f104b13cacf70d24f963ed25233d3ad936f4b3e
Logan Wessel <lgwessel812@gmail.com>
2019-11-08 15:25:33 -0600

    also backups nether and end for paper

--- Backup.bat
+++ Backup.bat
@@ -4,5 +4,7 @@ SET itdate=%itdate:~6,4%-%itdate:~3,2%-%itdate:~0,2%
 SET hour=%time:~0,2%
 IF "%hour:~0,1%" == " " SET hour=0%hour:~1,1%
 echo Current date: %itdate%-%hour%
-xcopy /e /c /h /i /v /r /y /q World Backup\%itdate%-%hour%\
+xcopy /e /c /h /i /v /r /y /q world Backup\%itdate%-%hour%-world\
+xcopy /e /c /h /i /v /r /y /q world_nether Backup\%itdate%-%hour%-world_nether\
+xcopy /e /c /h /i /v /r /y /q world_the_end Backup\%itdate%-%hour%-world_the_end\
 echo Backup Complete (assuming no errors above).
